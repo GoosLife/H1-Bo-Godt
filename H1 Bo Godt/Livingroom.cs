c893d55e27d884a66dedd467045b3825cb17247f
GoosLife <72657308+GoosLife@users.noreply.github.com>
2022-09-07 16:35:18 +0200

    Added livingroom and the last window classes

---
+++ H1 Bo Godt/Livingroom.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Bo_Godt
+{
+    internal class Livingroom : Room, IChangeWindows
+    {
+        private readonly Window staticWindow;
+        public Livingroom(int size, List<Window> windows, List<Door> doors, Window staticWindow) : base(size, windows, doors)
+        {
+            this.staticWindow = staticWindow;
+        }
+
+        public Window StaticWindow { get { return staticWindow; } }
+
+        public void ChangeWindow(int index, Window newWindow)
+        {
+            windows[index] = newWindow;
+        }
+    }
+}
